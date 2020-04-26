541897d64fec47ea28effc8eb52916418e7e2750
ky-s <kyohei_86@hotmail.com>
2020-04-26 23:19:12 +0900

    [update] add Task Form

---
+++ src/renderer/components/TaskForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="field has-addons">
+      <div class="control">
+          <label class="label">Project</label>
+        <input class="input" type="text" placeholder="project">
+      </div>
+      <div class="control">
+        <label class="label">Priority</label>
+        <div class="select">
+          <select>
+            <option value="null"></option>
+            <option value="H">High</option>
+            <option value="M">Middle</option>
+            <option value="L">Low</option>
+          </select>
+        </div>
+      </div>
+      <div class="control">
+        <label class="label">Tags</label>
+        <input class="input" type="text" placeholder="tags">
+      </div>
+    </div>
+
+    <div class="field has-addons">
+      <div class="control">
+        <label class="label">Due</label>
+        <input class="input" type="text" placeholder="Due">
+      </div>
+      <div class="control is-expanded">
+        <label class="label">Description</label>
+        <input class="input" type="text" placeholder="description">
+      </div>
+      <div class="control">
+        <label class="label">Create</label>
+        <button class="button">Create</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tasks-form'
+}
+</script>
--- src/renderer/pages/MainPage.vue
+++ src/renderer/pages/MainPage.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <h1 class="title">Tasks</h1>
+    <task-form/>
 
     <task-table :tasks="tasks"/>
 
@@ -8,12 +9,14 @@
 </template>
 
 <script>
+import TaskForm from '@/components/TaskForm.vue'
 import TaskTable from '@/components/TaskTable.vue'
 
 export default {
   name: 'main-page',
   components: {
-    TaskTable
+    TaskTable,
+    TaskForm
   },
   data: () => {
     const { getTasks } = require('@/../modules/taskwarrior')
