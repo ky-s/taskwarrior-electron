4ef4fb68f2fe2a2eb4537bf06d92600c516892d6
ky-s <kyohei_86@hotmail.com>
2020-04-29 22:04:02 +0900

    [fix] project-page re-render bug

--- src/renderer/pages/ProjectPage.vue
+++ src/renderer/pages/ProjectPage.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="container">
+  <div class="container" v-model="options">
     <h1 class="title">
-      <router-link to="/">Tasks</router-link> > {{ this.$route.params.project }}
+      <router-link to="/">Tasks</router-link> > {{ options.project }}
     </h1>
     <project-links />
 
-    <task-board :options="{ project: this.$route.params.project }"/>
+    <task-board :options="options" :key="options.project" />
   </div>
 </template>
 
@@ -18,6 +18,18 @@ export default {
   components: {
     ProjectLinks,
     TaskBoard
+  },
+  data: function () {
+    return {
+      options: {
+        project: this.$route.params.project
+      }
+    }
+  },
+  watch: {
+    '$route' (to, from) {
+      this.options.project = to.params.project
+    }
   }
 }
 </script>
