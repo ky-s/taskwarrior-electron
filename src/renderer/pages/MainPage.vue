97f302964e1d2c32746213f710ea28f32aaac2fd
ky-s <kyohei_86@hotmail.com>
2020-04-29 15:06:40 +0900

    [update] add refresh button

--- src/renderer/pages/MainPage.vue
+++ src/renderer/pages/MainPage.vue
@@ -4,7 +4,7 @@
 
     <task-form @reloadTask="reloadTask" />
 
-    <div class="tabs is-fullwidth is-boxed" v-model="activeTab" style="padding-top: 10px">
+    <div class="tabs is-fullwidth is-boxed" v-model="activeTab" style="padding-top: 20px">
       <ul>
         <li :class="activeTab == 'todo' ? 'is-active' : ''" v-on:click="activeTab = 'todo'">
           <a>Todo</a>
@@ -14,6 +14,10 @@
         </li>
       </ul>
     </div>
+
+    <button class="button is-light is-pulled-right" v-on:click="reloadTask()">
+      Refresh
+    </button>
 
     <div :class="activeTab == 'todo' ? '' : 'is-hidden'">
       <button class="button is-dark" v-on:click="redue()">
@@ -27,7 +31,7 @@
     </div>
 
     <div :class="activeTab == 'done' ? '' : 'is-hidden'">
-      <task-table :tasks="doneTasks" @reloadTask="reloadTask" :class="true ? '' : 'is-hidden'" />
+      <task-table :tasks="doneTasks" @reloadTask="reloadTask" />
     </div>
 
   </div>
